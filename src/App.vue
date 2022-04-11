7a51351a5120f7385cd8ade9cf0be816d0e5b2c7
morgan4080 <morganmurungi@live.com>
2022-04-11 18:27:22 +0300

    navbar0

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import AppNav from './components/AppNav.vue'
+</script>
+
+<template>
+<AppNav logo="" list-items="[]"></AppNav>
+</template>
+
+<style>
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
